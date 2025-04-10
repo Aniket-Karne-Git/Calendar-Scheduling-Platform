211510796fd76d0ba2a51c0e92419c35ed4ecfa8
Aniket-Karne-Git <ankarne@paktolus.com>
2025-04-10 12:35:21 +0530

    added auth functionality

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+import { requireUser } from "../lib/hooks";
+
+const Dashboard = async () => {
+  const session = await requireUser();
+  return <div>Dashboard</div>;
+};
+
+export default Dashboard;
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
-  export default function Home() {
-  return <h1>Hello World</h1>;
+import { Navbar } from "./components/Navbar";
+
+export default function Home() {
+  return (
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <Navbar />
+    </div>
+  );
 }
